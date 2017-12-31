29bcc490e2fe3b6d8b80890de6bffe5ed5a68192
Rômulo de Lacerda Corrêa Rímulo <romulolacerda@MacBook-Pro-de-Romulo.local>
2017-12-31 13:07:33 -0200

    Refactor using React Components instead pure Bootstrap and font-awesome classes

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -1,5 +1,13 @@
 import React from 'react'
-
+import Grid from '../template/grid'
+import IconButton from '../template/iconButton'
 export default props => (
-    <h1>Form</h1>
+    <div role='form' className='todoForm'>
+        <Grid cols='12 9 10'>
+            <input id='description' className='form-control' placeholder='Adicione uma tarefa'/>
+        </Grid>
+        <Grid cols='12 3 2'>
+            <IconButton style='primary' icons='plus'></IconButton>
+        </Grid>
+    </div>
 )
