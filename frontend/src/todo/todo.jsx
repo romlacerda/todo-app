f838146cd5e002389c9fe0dfa66b1ca46f483967
Rômulo de Lacerda Corrêa Rímulo <romulolacerda@MacBook-Pro-de-Romulo.local>
2018-01-06 18:16:03 -0200

    End of React, next commit = starting with Redux

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -15,15 +15,25 @@ export default class Todo extends Component {
         
         //Amarro o this ao componente
         this.handleAdd = this.handleAdd.bind(this)
+        this.handleSearch = this.handleSearch.bind(this)
         this.handleChange = this.handleChange.bind(this)
         this.handleRemove = this.handleRemove.bind(this)
+        this.handleMarkAsDone = this.handleMarkAsDone.bind(this)
+        this.handleMarkAsPending = this.handleMarkAsPending.bind(this)
+        this.handleClear = this.handleClear.bind(this)
         this.refresh()
     }
     
-    refresh() {
-        axios.get(`${URL}?sort=-createdAt`).then(resp => this.setState({...this.state, description: '', list: resp.data}))
+    refresh(description = '') {
+        const search = description ? `&description__regex=/${description}/` : ''
+        axios.get(`${URL}?sort=-createdAt${search}`)
+            .then(resp => this.setState({...this.state, description, list: resp.data}))
     }
 
+    handleSearch() {
+        this.refresh(this.state.description)
+    }
+    
 
     handleChange(e) {
         this.setState({...this.state, description: e.target.value})
@@ -35,17 +45,36 @@ export default class Todo extends Component {
     }
 
     handleRemove(todo) {
-        axios.delete(`${URL}/${todo._id}`).then(resp => this.refresh())
+        axios.delete(`${URL}/${todo._id}`).then(resp => this.refresh(this.state.description))
     }
 
+    handleMarkAsDone(todo) {
+        axios.put(`${URL}/${todo._id}`, {...todo, done: true}).then(resp => this.refresh(this.state.description))
+    }
+
+    handleMarkAsPending(todo) {
+        axios.put(`${URL}/${todo._id}`, {...todo, done: false}).then(resp => this.refresh(this.state.description))
+    }
+
+    handleClear() {
+        this.refresh()
+    }
     render() {
         return (
             <div>
                 <PageHeader name="Tarefas" small="Cadastro"></PageHeader>
-                <TodoForm description={this.state.description} handleAdd={this.handleAdd}
-                handleChange={this.handleChange} />
-                <TodoList list={this.state.list} 
+                <TodoForm description={this.state.description} 
+                handleAdd={this.handleAdd}
+                handleChange={this.handleChange} 
+                handleSearch={this.handleSearch}
+                handleClear={this.handleClear}
+                />
+                <TodoList 
+                    list={this.state.list} 
+                    handleMarkAsDone={this.handleMarkAsDone}
+                    handleMarkAsPending={this.handleMarkAsPending}
                     handleRemove={this.handleRemove}
+
                 />
             </div>
         )
--- frontend/src/todo/todoList.jsx
+++ frontend/src/todo/todoList.jsx
@@ -9,9 +9,13 @@ export default props => {
         const list = props.list || []
         return list.map(todo => (
             <tr key={todo._id}>
-                <td>{todo.description}</td>
+                <td className={todo.done ? 'markedAsDone' : ''}>{todo.description}</td>
                 <td>
-                    <IconButton style='danger' icon='trash-o' 
+                    <IconButton style='success' icon='check' hide={todo.done}
+                    onClick={() => props.handleMarkAsDone(todo)}></IconButton>
+                    <IconButton style='warning' icon='undo' hide={!todo.done}
+                    onClick={() => props.handleMarkAsPending(todo)}></IconButton>
+                    <IconButton style='danger' icon='trash-o' hide={!todo.done}
                     onClick={() => props.handleRemove(todo)}></IconButton>
                 </td>
             </tr>
@@ -23,7 +27,7 @@ export default props => {
             <thead>
                 <tr>
                     <th>Descrição</th>
-                    <th>Ações</th>
+                    <th className='tableActions'>Ações</th>
                 </tr>
             </thead>
             <tbody>
